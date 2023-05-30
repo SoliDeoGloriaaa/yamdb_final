c7d3a70226dfd8a532d61ad6e92acee6a2de3e8c
Александр Афанасьев <knowthepain00@mail.ru>
2023-05-30 22:43:08 +0300

    финал??

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -56,7 +56,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-# Database
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
