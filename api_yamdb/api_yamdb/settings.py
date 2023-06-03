bfb657f5cba4fbcaa6d649e33e8bf9f4304da232
Александр Афанасьев <knowthepain00@mail.ru>
2023-06-03 13:03:57 +0300

    bvf

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 # install
 INSTALLED_APPS = [
